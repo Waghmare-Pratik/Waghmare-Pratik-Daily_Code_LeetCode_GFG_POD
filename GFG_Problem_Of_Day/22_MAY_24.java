9df2f7f206c0c85a5a1b1b737f350b88eba7b246
Pratik Waghmare <145656673+Waghmare-Pratik@users.noreply.github.com>
2024-05-22 23:05:59 +0530

    Add files via upload

---
+++ GFG_Problem_Of_Day/22_MAY_24.java
@@ -0,0 +1,95 @@
+/*
+ * We have a horizontal number line. On that number line, we have gas stations at positions stations[0], stations[1], ..., stations[N-1], where n = size of the stations array. Now, we add k more gas stations so that d, the maximum distance between adjacent gas stations, is minimized. We have to find the smallest possible value of d. Find the answer exactly to 2 decimal places.
+
+Example 1:
+
+Input:
+n = 10
+stations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+k = 9
+Output: 0.50
+Explanation: Each of the 9 stations can be added mid way between all the existing adjacent stations.
+Example 2:
+
+Input:
+n = 10
+stations = [3,6,12,19,33,44,67,72,89,95] 
+k = 2 
+Output: 14.00 
+Explanation: Construction of gas stations at 8th(between 72 and 89) and 6th(between 44 and 67) locations.
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findSmallestMaxDist() which takes a list of stations and integer k as inputs and returns the smallest possible value of d. Find the answer exactly to 2 decimal places.
+
+Expected Time Complexity: O(n*log k)
+Expected Auxiliary Space: O(1)
+
+Constraint:
+10 <= n <= 5000 
+0 <= stations[i] <= 109 
+0 <= k <= 105
+ */
+
+ //{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+            Solution obj = new Solution();
+            double ans = obj.findSmallestMaxDist(a, k);
+            System.out.printf("%.2f", ans);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int noOfGasStationsBetween(int[] arr, double dist) {
+        int cnt = 0, n = arr.length;
+    
+        for (int i = 0; i < n - 1; i++) {
+          double stationsInBetween = (arr[i + 1] - arr[i]) / dist;
+          cnt += stationsInBetween;
+        }
+    
+        return cnt;
+    }
+
+    public static double findSmallestMaxDist(int[] arr, int k) {
+        int n = arr.length;
+        double maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n - 1; i++) maxi =
+          Math.max(maxi, (double) (arr[i + 1] - arr[i]));
+    
+        double low = 0, high = maxi, diff = 1e-6; // 10^-6
+        while(high - low > diff) {
+            double mid = low + (high - low) / 2;
+            if(noOfGasStationsBetween(arr, mid) <= k)
+                high=mid;
+            else    
+                low=mid;
+        }
+        
+        return high;
+    }
+};
