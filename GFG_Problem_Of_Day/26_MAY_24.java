b8badabb7fcd1e70d84971c97d976a3358ccc095
Pratik Waghmare <145656673+Waghmare-Pratik@users.noreply.github.com>
2024-05-26 22:21:25 +0530

    Add files via upload

---
+++ GFG_Problem_Of_Day/26_MAY_24.java
@@ -0,0 +1,87 @@
+/*
+ Given two strings x and y, and two values costX and costY, the task is to find the minimum cost required to make the given two strings identical. You can delete characters from both the strings. The cost of deleting a character from string X is costX and from Y is costY. The cost of removing all characters from a string is the same.
+
+Example 1:
+
+Input: x = "abcd", y = "acdb", costX = 10 
+       costY = 20.
+Output: 30
+Explanation: For Making both strings 
+identical we have to delete character 
+'b' from both the string, hence cost 
+will be = 10 + 20 = 30.
+Example 2:
+
+Input : x = "ef", y = "gh", costX = 10
+        costY = 20.
+Output: 60
+Explanation: For making both strings 
+identical, we have to delete 2-2 
+characters from both the strings, hence 
+cost will be = 10 + 10 + 20 + 20 = 60.
+Your Task:
+You don't need to read or print anything. Your task is to complete the function findMinCost() which takes both strings and the costs as input parameters and returns the minimum cost.
+
+Expected Time Complexity: O(|x|*|y|)
+Expected Space Complexity: O(|x|*|y|)
+
+Constraints:
+1 ≤ |x|, |y| ≤ 1000
+1<= costX, costY <= 105
+
+
+Solution
+
+ */
+
+ //{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String X = sc.next();
+            String Y = sc.next();
+            int costX = sc.nextInt();
+            int costY = sc.nextInt();
+
+            Solution ob = new Solution();
+            System.out.println(ob.findMinCost(X, Y, costX, costY));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int findMinCost(String x, String y, int costX, int costY) {
+        int n = x.length();
+        int m = y.length();
+        
+        // Create a 2D array to store the lengths of LCS
+        int[][] dp = new int[n + 1][m + 1];
+        
+        // Fill the dp tableA
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        int lcsLength = dp[n][m];
+        int minCost = (n - lcsLength) * costX + (m - lcsLength) * costY;
+        
+        return minCost;
+    }
+}
+
+
